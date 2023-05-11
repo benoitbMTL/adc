f86fce8404b6c8ab8b400637658df8a152c813a3
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-05-10 22:17:25 -0400

    bla

--- test.sh
+++ test.sh
@@ -3,9 +3,15 @@
 do_curl() {
     local HOST=$1
     echo "$HOST"
-    if true
-    then
-        curl -k https://"$HOST"
+    if [ true ] ; then
+        curl -s -o /dev/null "http://${HOST}" \
+            -H "authority: ${HOST}" \
+            -H "cache-control: max-age=0" \
+            -H "content-type: application/x-www-form-urlencoded" \
+            -H "origin: http://${HOST}" \
+            -H "referer: http://${HOST}" \
+            -H "user-agent: Bonjour" \
+            --insecure 
     fi
 }
 
