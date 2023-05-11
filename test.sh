3f9e62fb30f93c0cb5192b08db26924a715de30e
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-05-10 22:22:32 -0400

    test

--- test.sh
+++ test.sh
@@ -1,18 +1,46 @@
+# bbuonassera May 11th, 2023
+# ADC demo
 #!/bin/bash
 
+###############################################
+## Ensure we are running under bash
+###############################################
+if [ "$BASH_SOURCE" = "" ]; then
+    /bin/bash "$0"
+    exit 0
+fi
+
+###############################################
+## Set Variables
+###############################################
+VIP_DVWA="10.163.7.32"
+VIP_PETSTORE="10.163.7.33"
+VIP_SPEEDTEST="10.163.7.34"
+VIP_SHOP="10.163.7.35"
+VIP_HELLO="10.163.7.36"
+VIP_FINANCE="10.163.7.37"
+USER_AGENT="FortiADC Demo Script"
+
+############################################################################
+## EXECUTE CURL
+############################################################################
 do_curl() {
     local HOST=$1
-    echo "$HOST"
-    if [ true ] ; then
-        curl -s -o /dev/null "http://${HOST}" \
-            -H "authority: ${HOST}" \
-            -H "cache-control: max-age=0" \
-            -H "content-type: application/x-www-form-urlencoded" \
-            -H "origin: http://${HOST}" \
-            -H "referer: http://${HOST}" \
-            -H "user-agent: Bonjour" \
-            --insecure 
-    fi
+    local PATH=$2
+    local REFERER=$3
+    local DATA_RAW=$4
+    local COOKIE_FILE=$5
+
+    # Debug
+    echo "curl http://${HOST}/${PATH} --data-raw ${DATA_RAW}"
+
+    curl -v http://perdu.com
 }
 
-do_curl "perdu.com"
+
+############################################################################
+## Traffic Generator
+############################################################################
+
+do_curl "${VIP_DVWA}" "login.php" "" "username=pablo&password=letmein&Login=Login" "cookie.txt"
+do_curl "${VIP_DVWA}" "vulnerabilities/exec/" "index.php" "localhost" "cookie.txt"
